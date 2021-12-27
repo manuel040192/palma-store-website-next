6c7a02a9a5bd586d9ff80f4c7ee931ae25ad5f0c
Manuel Arce <manuel_040192@hotmail.com>
2021-12-26 23:19:22 -0500

    Update goods.js

--- pages/goods.js
+++ pages/goods.js
@@ -37,7 +37,7 @@ function Goods() {
                             <span style={{ position: 'absolute', top: 135, left: 140, color: '#ffffff', fontFamily: 'Rajdhani', fontSize: 28, fontWeight: 400 }}></span>    
                         </div>
                         <div style={{ position: 'relative' }}>
-                            <a href="#"><img src="https://firebasestorage.googleapis.com/v0/b/auth-development-b3b63.appspot.com/o/medias-azul-1.jpg?alt=media&token=81dc1661-d6d1-46d0-87c7-6438d4300e4e" style={{ width: 350, height: 310, objectFit: 'cover' }}/></a>
+                            <a href="/goods/gallery02"><img src="https://firebasestorage.googleapis.com/v0/b/auth-development-b3b63.appspot.com/o/medias-azul-1.jpg?alt=media&token=81dc1661-d6d1-46d0-87c7-6438d4300e4e" style={{ width: 350, height: 310, objectFit: 'cover' }}/></a>
                             <span style={{ position: 'absolute', top: 135, left: 155, color: '#ffffff', fontFamily: 'Rajdhani', fontSize: 28, fontWeight: 400 }}></span>    
                         </div>
                     </div>
