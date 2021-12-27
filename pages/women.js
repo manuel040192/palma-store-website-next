62c1e238bafbce7f77ecacf91ea0be771fb8b3a7
Manuel Arce <manuel_040192@hotmail.com>
2021-12-27 00:03:48 -0500

    Changes made

--- pages/women.js
+++ pages/women.js
@@ -33,7 +33,7 @@ function Women() {
                         }}
                     >
                         <div style={{ position: 'relative' }}>
-                            <a href="/"><img src="https://firebasestorage.googleapis.com/v0/b/auth-development-b3b63.appspot.com/o/cover-women.JPG?alt=media&token=ef5edd77-b8bb-46cc-b0ba-96f5ea58714c" style={{ width: 350, height: 310, objectFit: 'cover' }}/></a>
+                            <a href="/women/gallery01"><img src="https://firebasestorage.googleapis.com/v0/b/auth-development-b3b63.appspot.com/o/cover-women.JPG?alt=media&token=ef5edd77-b8bb-46cc-b0ba-96f5ea58714c" style={{ width: 350, height: 310, objectFit: 'cover' }}/></a>
                             <span style={{ position: 'absolute', top: 135, left: 140, color: '#ffffff', fontFamily: 'Rajdhani', fontSize: 28, fontWeight: 400 }}></span>    
                         </div>
                         <div style={{ position: 'relative' }}>
--- pages/women/gallery01.js
+++ pages/women/gallery01.js
@@ -1,4 +1,4 @@
-import Header from '../components/Navbar';
+import Header from '../../components/Navbar';
 
 function Women() {
     return (
