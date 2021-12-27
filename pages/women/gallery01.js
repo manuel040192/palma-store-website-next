b497dd9ef6e1cecd579a1cf75dfca16fa60c1404
Manuel Arce <manuel_040192@hotmail.com>
2021-12-26 23:56:25 -0500

    Changes made

---
+++ pages/women/gallery01.js
@@ -0,0 +1,46 @@
+import Header from '../components/Navbar';
+
+function Women() {
+    return (
+        <div>
+            <Header />
+            <div style={{
+                    display: 'flex',
+                    position: 'relative',
+                    height: '486px',
+                    width: '100%',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    flexDirection: 'column',
+                    paddingTop: '100px',
+                }}
+            >
+                <div style={{
+                        width: '1086px',
+                        display: 'flex',
+                        maxWidth: '1200px',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        flexDirection: 'column',
+                        height: '1336px',
+                    }}
+                >
+                    <div style={{
+                            width: '100%',
+                            display: 'grid',
+                            gridGap: 30,
+                            gridTemplateColumns: '1fr 1fr 1fr',
+                        }}
+                    >
+                        <div style={{ position: 'relative' }}>
+                            <a href="/"><img src="https://firebasestorage.googleapis.com/v0/b/auth-development-b3b63.appspot.com/o/cover-women.JPG?alt=media&token=ef5edd77-b8bb-46cc-b0ba-96f5ea58714c" style={{ width: 350, height: 310, objectFit: 'cover' }}/></a>
+                            <span style={{ position: 'absolute', top: 135, left: 140, color: '#ffffff', fontFamily: 'Rajdhani', fontSize: 28, fontWeight: 400 }}></span>    
+                        </div>                                       
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Women
