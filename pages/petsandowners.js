356f0e04cdebedef99767afd8cb985d19adc0d75
Manuel Arce <manuel_040192@hotmail.com>
2021-12-26 23:04:15 -0500

    Changes made

--- pages/petsandowners.js
+++ pages/petsandowners.js
@@ -33,7 +33,11 @@ function PetsAndOwners() {
                         }}
                     >
                         <div style={{ position: 'relative' }}>
-                            <a href="/petsandowners/gallery01"><img src="https://firebasestorage.googleapis.com/v0/b/auth-development-b3b63.appspot.com/o/cover-petsandowners.JPEG?alt=media&token=7e3b4c72-b34c-4718-8431-4412e5d845f2" style={{ width: 350, height: 310, objectFit: 'cover' }}/></a>
+                            <a href="/"><img src="https://firebasestorage.googleapis.com/v0/b/auth-development-b3b63.appspot.com/o/cover-petsandowners.JPEG?alt=media&token=7e3b4c72-b34c-4718-8431-4412e5d845f2" style={{ width: 350, height: 310, objectFit: 'cover' }}/></a>
+                            <span style={{ position: 'absolute', top: 135, left: 140, color: '#ffffff', fontFamily: 'Rajdhani', fontSize: 28, fontWeight: 400 }}></span>    
+                        </div>
+                        <div style={{ position: 'relative' }}>
+                            <a href="/petsandowners/gallery02"><img src="https://firebasestorage.googleapis.com/v0/b/auth-development-b3b63.appspot.com/o/pets-gris-1.JPEG?alt=media&token=c04e94a4-3953-444b-8e95-31dfbf5bed37" style={{ width: 350, height: 310, objectFit: 'cover' }}/></a>
                             <span style={{ position: 'absolute', top: 135, left: 140, color: '#ffffff', fontFamily: 'Rajdhani', fontSize: 28, fontWeight: 400 }}></span>    
                         </div>      
                     </div>
